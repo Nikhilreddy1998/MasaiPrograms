bff2985022508ef5af2dd4be467e10c63a4a554b
Nikhilreddy1998 <nikhilreddy125@gmail.com>
2025-05-11 18:34:08 +0530

    file added

--- Intro_to_Vite_and_react_lifecycle/14thq/my-react-app/src/App.jsx
+++ Intro_to_Vite_and_react_lifecycle/14thq/my-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import './App.css';
 
 function App() {
- // I will be completing this in few days. Right now preparing for PAI
+ // I will be completing this by Monday eod
 }
 
 export default App;
