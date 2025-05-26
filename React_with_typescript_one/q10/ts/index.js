c3fd0b60d980d2a78899ba1d55cd994d8f349bc2
Nikhilreddy1998 <nikhilreddy125@gmail.com>
2025-05-26 17:54:11 +0530

    files added

---
+++ React_with_typescript_one/q10/ts/index.js
@@ -0,0 +1,3 @@
+"use strict";
+//I am having revision week before PAI-II i will be completing all my revision 
+// and all assignments before the end of this week.
---
+++ React_with_typescript_one/q8/ts/index.ts
@@ -0,0 +1,2 @@
+//I am having revision week before PAI-II i will be completing all my revision 
+// and all assignments before the end of this week.
