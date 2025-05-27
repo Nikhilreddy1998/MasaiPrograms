be5a1171300aefe5b7edaef285c7e9305611891f
Nikhilreddy1998 <nikhilreddy125@gmail.com>
2025-05-28 02:16:29 +0530

    file added

--- Modern_redux_with_redux_toolkit/q3/my-react-app/src/App.jsx
+++ Modern_redux_with_redux_toolkit/q3/my-react-app/src/App.jsx
@@ -1,9 +1,13 @@
 import React, { useRef } from 'react';
 import './App.css';
+import Counter from './counter';
 
 function App() {
- // we are given revision week i am revising for PAI before entering into unit-5
- // I will be completing this assignmnet by the end of revisoin week unit-4
+ return(
+    <>
+    <Counter/>
+    </>
+ )
 }
 
 export default App;
