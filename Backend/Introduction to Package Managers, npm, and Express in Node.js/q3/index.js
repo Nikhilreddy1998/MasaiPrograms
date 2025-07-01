7179e9c213b77dd39109933ac89ab5bb6d7c19f0
Nikhilreddy1998 <nikhilreddy125@gmail.com>
2025-07-01 21:51:16 +0530

    files added

--- Backend/Introduction to Package Managers, npm, and Express in Node.js/q3/index.js
+++ Backend/Introduction to Package Managers, npm, and Express in Node.js/q3/index.js
@@ -1,18 +1,22 @@
-const express =require('express')
-const app =express()
-const readDataFile=require('./read')
-app.get('/test',(req,res)=>{
-    res.send("Test route is working")
-})
-app.get('/readfile',(req,res)=>{
-    readDataFile((err, data) => {
-        if (err) {
-            return res.status(500).send("Error reading file.");
-        }
-        res.send(`File content: ${data}`); // Send the data once it's available
-    });
+const express=require('express')
+const app=express()
+app.use(express.json())
+
+const rateLimit = require('express-rate-limit')
+
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, 
+	limit: 5, 
+	standardHeaders: 'draft-8',
+	legacyHeaders: false, 
 })
 
+app.use(limiter)
+
 app.listen(3000,()=>{
-    console.log("server started running")
+    console.log("server started")
+})
+
+app.get('/info',(req,res)=>{
+res.json({msg:'this is info'})
 })
